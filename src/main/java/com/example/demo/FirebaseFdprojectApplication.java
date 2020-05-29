7babcb5d1b1f872dca08a45fa4391e50a935a90e
rajshah1 <shahraj121997@gmail.com>
2020-05-29 12:05:56 +0530

    8 checking

--- src/main/java/com/example/demo/FirebaseFdprojectApplication.java
+++ src/main/java/com/example/demo/FirebaseFdprojectApplication.java
@@ -3,14 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 public class FirebaseFdprojectApplication {
 
