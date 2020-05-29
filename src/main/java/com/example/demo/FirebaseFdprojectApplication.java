57537898de0f752c09679813ab5468c5785a8cd6
rajshah1 <shahraj121997@gmail.com>
2020-05-29 16:33:50 +0530

    5. all working in heroku

--- src/main/java/com/example/demo/FirebaseFdprojectApplication.java
+++ src/main/java/com/example/demo/FirebaseFdprojectApplication.java
@@ -1,11 +1,10 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,10 +16,9 @@ public class FirebaseFdprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirebaseFdprojectApplication.class, args);
 	}
-	
-	@GetMapping("/")
+	@RequestMapping("/home")
 	public String home() {
-		return "Raj snab";
+		return "Hello World";
 	}
 
 }
