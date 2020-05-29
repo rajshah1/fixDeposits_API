01b90df6fb964da19adfa2f81e55cd5015d68a42
rajshah1 <shahraj121997@gmail.com>
2020-05-29 11:09:51 +0530

    6.Added Google Cred for firebase Heroku

--- src/main/java/com/example/demo/FirebaseFdprojectApplication.java
+++ src/main/java/com/example/demo/FirebaseFdprojectApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 
