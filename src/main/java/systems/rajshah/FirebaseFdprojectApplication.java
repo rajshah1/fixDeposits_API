7a1c5a93f2b2553775d3024ce5f59053d8a3866b
rajshah1 <shahraj121997@gmail.com>
2020-06-01 19:02:46 +0530

    treet

--- src/main/java/systems/rajshah/FirebaseFdprojectApplication.java
+++ src/main/java/systems/rajshah/FirebaseFdprojectApplication.java
@@ -3,15 +3,12 @@ package systems.rajshah;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
+
 public class FirebaseFdprojectApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
