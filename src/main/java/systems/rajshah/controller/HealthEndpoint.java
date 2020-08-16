7498bdcf00b5bcc0047f2835e742ca55ffeaa903
rajshah1 <raj.shah@infosys.com>
2020-08-16 23:12:50 +0530

    45. Health check Updated

---
+++ src/main/java/systems/rajshah/controller/HealthEndpoint.java
@@ -0,0 +1,18 @@
+package systems.rajshah.controller;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("HeathEndPointMain")
+public class HealthEndpoint implements HealthIndicator{
+
+	@Override
+	@GetMapping("/health")
+	public Health health() {
+		// TODO Auto-generated method stub
+		return Health.up().build();
+	}
+
+}
